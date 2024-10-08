2a3f57348c804b3cd05cef83cf838feb2276f84f
Connell Reffo <connell.reffo@ucalgary.ca>
2024-10-07 20:13:44 -0600

    Some conceptual modelling (mainly backup)

--- QSPI/QSPI_Driver.hpp
+++ QSPI/QSPI_Driver.hpp
@@ -1,6 +1,6 @@
 /**
  * @author Connell Reffo
- * @brief This driver is specific to STM32H743XX chips
+ * @brief This driver is specific to STM32H7xx chips
  *
  * To set up correctly in VS Code, add the following to c_cpp_properties.json:
  *
@@ -21,6 +21,29 @@
 #include <stm32h743xx.h>
 #include <stm32h7xx_hal_qspi.h>
 
-class QSPIDriver {};
+struct QSPIConfig {
+ private:
+  int frequency;
+  int mode;
+  int address_size;
+
+ public:
+  QSPIConfig(int frequency, int mode, int address_size)
+      : frequency(frequency), mode(mode), address_size(address_size) {}
+
+  int get_frequency() const { return this->frequency; }
+
+  int get_mode() const { return this->mode; }
+
+  int get_address_size() const { return this->address_size; }
+};
+
+class QSPIDriver {
+ private:
+  QSPI_HandleTypeDef *qspi_handle;
+
+ public:
+  QSPIDriver(QSPI_HandleTypeDef *hqspi) : qspi_handle(hqspi) {}
+};
 
 #endif  // QSPI_DRIVER_H
